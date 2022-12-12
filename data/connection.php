bd7f8e6ed06c00bc71c86128f33f459ebc6c6d8b
adswebwork <spence@s24defi.com>
2022-12-11 19:02:00 -0500

    baseline code - will be considered ground floor

--- data/connection.php
+++ data/connection.php
@@ -3,8 +3,11 @@ $db_host = 'mysql.DATABASE.com';
 $db_user = 'user';
 $db_pass = 'password';
 $db_name = 'db';
-$conn = mysqli_connect($db_host,$db_user,$db_pass) or die("Unable to connect to database.");
+$conn = mysqli_connect($db_host, $db_user, $db_pass) or die("Unable to connect to database.");
 mysqli_select_db($conn, $db_name) or die("Unable to open database.");
-if($conn){print 'success';} print 'fail';
+if ($conn) {
+  print 'success';
+}
+print 'fail';
 
 
---
+++ data/formProcess.php
@@ -0,0 +1,5 @@
+<?php
+
+http_redirect('../index.html?status=submitted');
+
+
